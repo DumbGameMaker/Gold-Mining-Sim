55fb06d500e25e6918b37110a0b40d31b129dafd
DumbGameMaker <DumbGameMaker@users.noreply.github.com>
2020-03-26 17:15:22 -0700

    Initial Commit

---
+++ build/precache-manifest.6404d127c8c867c252e42d6c800659e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b309e55dbdb79076b76e3f02b4d3bf7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a299eba6310e887cacd6",
+    "url": "/static/css/main.181a866f.chunk.css"
+  },
+  {
+    "revision": "ac6e77d9a59f0f2e089a",
+    "url": "/static/js/2.96d99ccc.chunk.js"
+  },
+  {
+    "revision": "b9a68ff2a8681781e77e3c61136482e8",
+    "url": "/static/js/2.96d99ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a299eba6310e887cacd6",
+    "url": "/static/js/main.209739f4.chunk.js"
+  },
+  {
+    "revision": "4fdf01d0e82c8625b667",
+    "url": "/static/js/runtime-main.8aa6a2ef.js"
+  }
+]);
